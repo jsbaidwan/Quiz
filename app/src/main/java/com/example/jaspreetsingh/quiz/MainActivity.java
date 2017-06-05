74da9d2b33e97a45e931a05804fc3559c8d53a10
Jaspreet Singh (DELL) <cgc.jsbaidwan@gmail.com>
2017-06-05 22:45:38 +0530

    successfully display the name of the participant

--- app/src/main/java/com/example/jaspreetsingh/quiz/MainActivity.java
+++ app/src/main/java/com/example/jaspreetsingh/quiz/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.jaspreetsingh.quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.R.attr.name;
 
 public class MainActivity extends AppCompatActivity {
 int score;
@@ -9,6 +15,38 @@ int score;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    /*
+    This method is to call when the submit button is clicked
+     */
+    public void submit(View v)  {
+        EditText nameField = (EditText) findViewById(R.id.participant_name);
+        String name = nameField.getText().toString();
+
+        String resultMessage = resultSummary(name);
+
+        displayResult(resultMessage);
+    }
+    /*
+    This method create the summary of the result
+     */
+    public String resultSummary(String name)   {
+        String resultMessage = "Name of the Participant is: " + name;
+        return resultMessage;
+    }
+
+//    public int Question1()  {
+//        CheckBox answer1 = (CheckBox) findViewById(R.id.mcq_prime_number_answer_8);
+//        Boolean isAnswer1 = answer1.isChecked();
+//        return isAnswer1;
+//    }
+    /*
+    This method display the result summary in the TextView
+     */
+    public void displayResult (String result)    {
+        TextView resultTextView = (TextView) findViewById(R.id.result_text_view);
+        resultTextView.setText(result);
+
     }
 
 }
